041c66e50a2fa3b4b8ef1a715beace0a72e14473
Mark Gavrilman <markgav19@gmail.com>
2024-12-28 21:07:00 +0200

    update

---
+++ devHTML/grid/example_09/index.js
@@ -0,0 +1,18 @@
+const main = document.querySelector('.contentsub');
+const main2 = document.querySelector('.contacts-wrapper');
+const scrollProgress = document.querySelector('.scroll-bar')
+
+const progress = () => {
+    const scroll = main.scrollTop;
+console.log("scrolling");
+    const height = main.scrollHeight - main.clientHeight;
+    const percent = (scroll / height) * 100;
+    scrollProgress.style.width = percent + "%";
+}
+
+const handleKeydown = (e) => {
+   console.log("keydown:"+e.key);
+}
+ //main.firstElementChild.focus()
+main.addEventListener('scroll', progress);
+//main.addEventListener('keydown', handleKeydown);
